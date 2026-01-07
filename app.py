73b53018622b4296ea8deb69579100b346dbaa93
moholandcom <160321238+moholandcom@users.noreply.github.com>
2026-01-07 21:34:46 +0330

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot is running!"
+    return "🤖 Bot is running!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
